9d6bfe2094820a1e00c72d35fdee9b6c432023ac
Tobibur <tobiburrahman786@gmail.com>
2018-08-13 23:51:30 +0530

    Fixed Typos

--- app/src/main/java/com/tobibur/readquotes/QuotesViewModel.kt
+++ app/src/main/java/com/tobibur/readquotes/QuotesViewModel.kt
@@ -1,9 +1,9 @@
 package com.tobibur.readquotes
 
 import android.annotation.SuppressLint
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
 import android.util.Log
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.JsonHttpResponseHandler
@@ -12,7 +12,7 @@ import cz.msebera.android.httpclient.Header
 import org.json.JSONObject
 
 class QuotesViewModel : ViewModel(){
-    private val QUOTE_URL = "http://api.forismatic.com/api/1.0/"
+    private val quoteURL = "http://api.forismatic.com/api/1.0/"
 
     private var quote: MutableLiveData<String>? = null
 
@@ -30,16 +30,16 @@ class QuotesViewModel : ViewModel(){
     private fun loadQuote() {
         val client = AsyncHttpClient()
 
-        client.get(QUOTE_URL, getParams(), object : JsonHttpResponseHandler(){
+        client.get(quoteURL, getParams(), object : JsonHttpResponseHandler(){
             @SuppressLint("SetTextI18n")
             override fun onSuccess(statusCode: Int, headers: Array<Header>, response: JSONObject) {
                 super.onSuccess(statusCode, headers, response)
-                val JsonResponse = response.toString()
+                val jsonResponse = response.toString()
                 val quoteText = response.getString("quoteText")
                 val author = response.getString("quoteAuthor")
                 val result = "$quoteText \n\n-$author"
                 quote!!.value = result
-                Log.i("MainActivity", "onSuccess: $JsonResponse")
+                Log.i("MainActivity", "onSuccess: $jsonResponse")
             }
 
             override fun onFailure(statusCode: Int, headers: Array<Header>, throwable: Throwable, errorResponse: JSONObject) {
